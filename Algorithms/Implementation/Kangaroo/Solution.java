0768ba2327b82641b7de183c7615702ee1ba802d
Ahmad Darwich <darw.ahmad@gmail.com>
2017-10-23 15:41:33 +0200

    Create Solution.java

---
+++ Algorithms/Implementation/Kangaroo/Solution.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static String kangaroo(int x1, int v1, int x2, int v2) {
+        // will never land at the same location
+        if (x1 != x2 && v1 < v2) return "NO";
+        
+        while (x1 <= x2){
+            if (x1 == x2) return "YES";
+            x1 += v1;
+            x2 += v2;
+        }
+        return "NO";
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x1 = in.nextInt();
+        int v1 = in.nextInt();
+        int x2 = in.nextInt();
+        int v2 = in.nextInt();
+        String result = kangaroo(x1, v1, x2, v2);
+        System.out.println(result);
+    }
+}
