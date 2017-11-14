fc683d10f895c37d315e14f68eb862bb0530c371
Ahmad Darwich <darw.ahmad@gmail.com>
2017-11-14 16:47:12 +0100

    Update Solution.java

--- Algorithms/Implementation/Non-Divisible Subset/Solution.java
+++ Algorithms/Implementation/Non-Divisible Subset/Solution.java
@@ -7,6 +7,23 @@ import java.util.regex.*;
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-    }
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] subSet = new int[k];
+        int resultat = 0;
+        for(int i=0; i < n; i++){
+            int reste = in.nextInt();
+            ++subSet[reste%k];
+        }  
+        for (int i = 0; i <= k/2  ; ++i){
+            int secondNumber = (k - i) % k;
+            if (i == secondNumber) {
+                resultat += Math.min(subSet[i],1);
+            }else {
+                resultat += Math.max(subSet[i],subSet[secondNumber]);
+            } 
+        }
+        System.out.println(resultat);
+        in.close();     }
 }
